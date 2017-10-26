2b46775d849d45eb1849b15fd0adadadf590296c
Stephen J Tierney III <stephent137@outlook.com>
2017-10-26 13:12:33 -0400

    Add project files.

---
+++ Martini/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Martini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
